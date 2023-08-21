4f5e47c1fb87dadcfd7d391d4f8fa4749011076b
Tamara Batyukova <tbatyukova@yandex.ru>
2023-08-21 16:02:59 +0400

    new test added

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class NewTest {
+import org.junit.jupiter.api.Test;
+
+public class NewTest {
+    @Test
+    void newTest(){
+        int i = 2;
+        while(i < 5){
+            System.out.println(i);
+            i++;
+        }
+    }
 }
