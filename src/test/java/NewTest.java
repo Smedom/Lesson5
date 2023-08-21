215ef76f62e1a65f095cb0b445fe40cf3ea8d827
Tamara Batyukova <tbatyukova@yandex.ru>
2023-08-21 16:33:27 +0400

    Changed i

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 public class NewTest {
     @Test
     void newTest(){
-        int i = 2;
+        int i = 7;
         while(i < 5){
             System.out.println(i);
             i++;
